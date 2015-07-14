7379d5b9d1df7e6c3214f71dc3e3c6ad299569a0
Errol A. Zuñiga Jr <errol.zuniga@coredna.com>
2015-07-14 21:32:54 +0800

    Updates hal

--- config/module.config.php
+++ config/module.config.php
@@ -594,14 +594,14 @@ return array(
         'controllers' => array(
             'CanariumCore\\V1\\Rest\\User\\Controller' => 'HalJson',
         ),
-        'accept-whitelist' => array(
+        'accept_whitelist' => array(
             'CanariumCore\\V1\\Rest\\User\\Controller' => array(
                 0 => 'application/vnd.canarium-core.v1+json',
                 1 => 'application/hal+json',
                 2 => 'application/json',
             ),
         ),
-        'content-type-whitelist' => array(
+        'content_type_whitelist' => array(
             'CanariumCore\\V1\\Rest\\User\\Controller' => array(
                 0 => 'application/json',
             ),
