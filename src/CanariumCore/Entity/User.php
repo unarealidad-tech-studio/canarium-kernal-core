b586db8dad24a45d6d95c5c1c1b90c559800a9da
Errol A. Zuñiga Jr <errol.zuniga@unarealidad.com>
2016-10-22 13:32:00 +0800

    Updates relationship

--- src/CanariumCore/Entity/User.php
+++ src/CanariumCore/Entity/User.php
@@ -64,7 +64,7 @@ class User implements UserInterface, ProviderInterface
      * @var \Doctrine\Common\Collections\Collection
      * @ORM\ManyToMany(targetEntity="CanariumCore\Entity\Role")
      * @ORM\JoinTable(name="user_role_linker",
-     *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
+     *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id", onDelete="CASCADE")},
      *      inverseJoinColumns={@ORM\JoinColumn(name="role_id", referencedColumnName="id")}
      * )
      */
